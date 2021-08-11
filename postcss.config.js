014514d147c7834d6bac4b2ed8dac49ad80bd53e
arctic_hen7 <arctic_hen7@pm.me>
2021-08-12 08:04:22 +1000

    build: 🔨 updated postcss for production

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,27 @@
-module.exports = {
-    plugins: [
-        require("postcss-import"),
-        require("tailwindcss/nesting"),
-        require("tailwindcss"),
+const plugins = [
+    require("postcss-import"),
+    require("tailwindcss/nesting"),
+    require("tailwindcss"),
+];
+if (process.env.NODE_ENV === "production") {
+    plugins.push(
         require("autoprefixer"),
-    ],
+        require("cssnano")({
+            preset: "default",
+        }),
+        require("@fullhuman/postcss-purgecss")({
+            content: [
+                "src/**/*.njk",
+                "src/**/*.md",
+                "src/**/*.html",
+                "src/**/*.css",
+            ],
+            defaultExtractor: (content) =>
+                content.match(/[\w-/:]+(?<!:)/g) || [],
+        })
+    );
+}
+
+module.exports = {
+    plugins,
 };
